2c8c66ec26300442758cd094d8c72488e7fd550d
“Raquel <raquelmichelonaulas@gmail.com>
2022-05-09 17:22:44 +0100

    navigation buttons

--- frontend/src/pages/Form/index.tsx
+++ frontend/src/pages/Form/index.tsx
@@ -1,4 +1,5 @@
 import "./styles.css";
+import { Link } from "react-router-dom";
 
 function Form() {
   const movie = {
@@ -40,7 +41,9 @@ function Form() {
             </button>
           </div>
         </form>
-        <button className="btn btn-primary moviestar-btn mt-3">Cancel</button>
+        <Link to="/">
+          <button className="btn btn-primary moviestar-btn mt-3">Cancel</button>
+        </Link>
       </div>
     </div>
   );
