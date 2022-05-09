2733ffc410568b267db9adcfff2c7f028b9ce32a
“Raquel <raquelmichelonaulas@gmail.com>
2022-05-09 14:05:44 +0100

    project created

---
+++ backend/src/main/java/com/raquelmichelon/moviestar/MoviestarApplication.java
@@ -0,0 +1,13 @@
+package com.raquelmichelon.moviestar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviestarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviestarApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/raquelmichelon/moviestar/MoviestarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raquelmichelon.moviestar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviestarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
