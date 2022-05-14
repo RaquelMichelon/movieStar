730298a7cac6642d7dfced2f9c524b30430dff50
“Raquel <raquelmichelonaulas@gmail.com>
2022-05-14 11:19:59 +0100

    pagination

--- frontend/src/components/Pagination/index.tsx
+++ frontend/src/components/Pagination/index.tsx
@@ -1,15 +1,35 @@
 import { ReactComponent as Arrow } from "assets/img/arrow.svg";
+import { MoviePage } from "types/movie";
 import "./styles.css";
 
-function Pagination() {
+type Props = {
+  page: MoviePage;
+  onChange: Function;
+};
+
+function Pagination({ page, onChange }: Props) {
   return (
     <div className="moviestar-pagination-container">
       <div className="moviestar-pagination-box">
-        <button className="moviestar-pagination-button" disabled={true}>
+        {/* button disabled if page were the first one */}
+        <button
+          className="moviestar-pagination-button"
+          disabled={page.first}
+          onClick={() => {
+            onChange(page.number - 1);
+          }}
+        >
           <Arrow />
         </button>
-        <p>{`${1} de ${3}`}</p>
-        <button className="moviestar-pagination-button" disabled={false}>
+        <p>{`${page.number + 1} de ${page.totalPages}`}</p>
+        {/* button disabled if page were the last one */}
+        <button
+          className="moviestar-pagination-button"
+          disabled={page.last}
+          onClick={() => {
+            onChange(page.number + 1);
+          }}
+        >
           <Arrow className="moviestar-flip-horizontal" />
         </button>
       </div>
